30915c61e9db4b205957b84e0ec9244cea47aa7b
Anh B. Tran <anhb.tran1010@gmail.com>
2018-09-25 19:42:45 -0500

    UI for the app

---
+++ SJU-Rock-Wall/SJU-Rock-Wall/GameViewController.swift
@@ -0,0 +1,77 @@
+//
+//  GameViewController.swift
+//  SJU-Rock-Wall
+//
+//  Created by Tran, Anh B on 9/24/18.
+//  Copyright © 2018 Tran, Anh B. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+        setupCamera()
+        spawnShape()
+    }
+    
+    func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    func setupView() {
+        scnView = self.view as! SCNView
+        // 1
+        scnView.showsStatistics = true
+        // 2
+        scnView.allowsCameraControl = true
+        // 3
+        scnView.autoenablesDefaultLighting = true
+    }
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+    }
+    
+    func setupCamera() {
+        // 1
+        cameraNode = SCNNode()
+        // 2
+        cameraNode.camera = SCNCamera()
+        // 3
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        // 4
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func spawnShape() {
+        // 1
+        var geometry:SCNGeometry
+        // 2
+        switch ShapeType.random() {
+        case .Box:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 1.0)
+        case .Sphere:
+            geometry = SCNSphere(radius: 0.5)
+        case .Pyramid:
+            geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
+        default:
+            // 3
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        }
+        // 4
+        let geometryNode = SCNNode(geometry: geometry)
+        // 5
+        scnScene.rootNode.addChildNode(geometryNode)
+    }
+}
