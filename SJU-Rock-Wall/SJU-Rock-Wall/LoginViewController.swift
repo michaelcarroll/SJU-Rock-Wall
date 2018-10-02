900e54a71a21d392c90f3c3801b00770c85aa00d
Carroll <mcarroll001@sjlc106l93543h.ad.csbsju.edu>
2018-10-01 20:42:55 -0500

    working login demo

--- SJU-Rock-Wall/SJU-Rock-Wall/LoginViewController.swift
+++ SJU-Rock-Wall/SJU-Rock-Wall/LoginViewController.swift
@@ -19,8 +19,30 @@ class LoginViewController: UIViewController {
         var email = emailField.text
         var password = passwordField.text
         
+        let json: [String: Any] = ["email": email!, "password": password!]
         
-    }
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // create post request
+        let url = URL(string: "http://152.65.36.36/login.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        // insert json data to the request
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+            }
+        }
+        
+        task.resume()    }
     
 }
 
