13c8d15f90c0b746292bd2269a46bcc240b2c19a
bkalsow <bdkalsow@yahoo.com>
2018-11-29 17:57:22 -0600

    Michael help plz

--- SJU-Rock-Wall/SJU-Rock-WallTests/SJU_Rock_WallTests.swift
+++ SJU-Rock-Wall/SJU-Rock-WallTests/SJU_Rock_WallTests.swift
@@ -46,9 +46,77 @@ class SJU_Rock_WallTests: XCTestCase
     {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        let uid: String
+         let json: [String: Any] = ["email": "jdoe@example.com", "password": "ThisIsATest"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // create post request
+        let url = URL(string: "http://sjurockwall.atwebpages.com/login.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        // insert json data to the request
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request)
+        {
+            data, response, error in
+            guard let data = data, error == nil else
+            {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            
+            if let responseJSON = responseJSON as? [String: Any]
+            {
+                print(responseJSON)
+                
+                let error = responseJSON["error"] as! Int
+                
+                
+                uid = responseJSON["message"]
+                
+                
+            }
+            
+        }
+       
+        let json2: [String: Any] = ["id": uid!]
+        let jsonData2 = try? JSONSerialization.data(withJSONObject: json2)
+        
+         let url2 = URL(string: "http://sjurockwall.atwebpages.com/deleteUser.php")!
+        var request2 = URLRequest(url: url2)
+        
+        request.httpMethod = "Post"
+        
+        request.httpBody = jsonData2
+        
+        let task2 = URLSession.shared.dataTask(with: request2)
+        {
+            data, response, error in
+            guard let data = data, error == nil else
+            {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any]
+            {
+                print(responseJSON)
+                
+                let error = responseJSON["error"] as! Int
+                
+                
+                let Id = responseJSON["message"] as! Int
+                
+                
+            }
+            
+        }
     }
     
-    func testLoginSuceeds()
+       func testLoginSuceeds()
     {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
